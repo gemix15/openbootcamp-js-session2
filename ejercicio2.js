79818dc2731fec7e0ae9266d16e0d2012395561b
Gemma Batlle <gembb15@gmail.com>
2022-06-27 11:13:38 +0200

    OpenBootcamp javascript ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,11 @@
+const fechaNacimiento = new Date(1991, 4, 11);
+const libro = {
+    titulo: "Dracula",
+    autor: "Bram Stoker",
+    fecha: new Date(1897, 4, 26),
+    url: "https://es.wikipedia.org/wiki/Dr%C3%A1cula" 
+};
+
+const yo = ["Gemma", 31, true, fechaNacimiento, libro];
+
+console.log(yo);
